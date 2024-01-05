481463b900c4db57753b24a6561750b777b2a5cd
princess-mikus <Kiankocasaubon01@gmail.com>
2024-01-05 04:09:06 +0100

    Found a really funny bug, lmao

--- date-calculator/main.py
+++ date-calculator/main.py
@@ -1,24 +1,23 @@
 # Made by Mikus
 
 import datetime
-import sys
-import decimal
+from sys import argv
 
 def main():
-	if len(sys.argv) != 3:
+	if len(argv) != 3:
 		print("Bad number of arguments"), exit(-1)
 
-	if sys.argv[1] == "today":
+	if argv[1] == "today":
 		date1 = str(datetime.date.today()).split('-')
 		date1.reverse()
 	else:
-		date1 = sys.argv[1].split('/')
+		date1 = argv[1].split('/')
 
-	if sys.argv[2] == "today":
+	if argv[2] == "today":
 		date2 = str(datetime.date.today()).split('-')
 		date2.reverse()
 	else:
-		date2 = sys.argv[2].split('/')
+		date2 = argv[2].split('/')
 
 	if len(date1) > 3 or len(date2) > 3:
 		print("Argument not a date"), exit (-1)
@@ -38,18 +37,18 @@ def main():
 
 	delta = date1 - date2
 
-	if sys.argv[1] != "today" and sys.argv[2] != "today":
-		if int(delta.days) < 0:
-			print(f"{sys.argv[1]} is {int(delta.days) * -1} days before {sys.argv[2]}")
-		elif int(delta.days) > 0:
-			print(f"{sys.argv[1]} is {int(delta.days)} days after {sys.argv[2]}")
+	if argv[1] != "today" and argv[2] != "today":
+		if int(delta.days) > 0:
+			print(f"{argv[1]} is {int(delta.days)} days before {argv[2]}")
+		elif int(delta.days) < 0:
+			print(f"{argv[1]} is {int(delta.days) * -1} days after {argv[2]}")
 		else:
 			print("Those are the same dates!")
 	else:
-		if int(delta.days) < 0:
-			print(f"That date will be in {int(delta.days) * -1} days")
-		elif int(delta.days) > 0:
-			print(f"That date was {int(delta.days)} days in the past")
+		if int(delta.days) > 0:
+			print(f"That date will be in {int(delta.days)} days")
+		elif int(delta.days) < 0:
+			print(f"That date was {int(delta.days) * -1} days in the past")
 		else:
 			print("Today is indeed today")
 
