5c6105e41a53a90750a30a8426f025519d9a8446
princess-mikus <Kiankocasaubon01@gmail.com>
2024-01-05 03:40:27 +0100

    Adding if __name__ == '__main__' to main script

--- date-calculator/main.py
+++ date-calculator/main.py
@@ -4,50 +4,54 @@ import datetime
 import sys
 import decimal
 
-if len(sys.argv) != 3:
-	print("Bad number of arguments"), exit(-1)
+def main():
+	if len(sys.argv) != 3:
+		print("Bad number of arguments"), exit(-1)
 
-if sys.argv[1] == "today":
-	date1 = str(datetime.date.today()).split('-')
-	date1.reverse()
-else:
-	date1 = sys.argv[1].split('/')
-
-if sys.argv[2] == "today":
-	date2 = str(datetime.date.today()).split('-')
-	date2.reverse()
-else:
-	date2 = sys.argv[2].split('/')
-
-if len(date1) > 3 or len(date2) > 3:
-	print("Argument not a date"), exit (-1)
-
-for i in range(0, len(date1)):
-	if not date1[i].isdigit():
-		print("Bad argument format")
-		exit (0)
-
-for i in range(0, len(date2)):
-	if not date2[i].isdigit():
-		print("Bad argument format")
-		exit (0)
-
-date1 = datetime.date(int(date1[2]), int(date1[1]), int(date1[0]))
-date2 = datetime.date(int(date2[2]), int(date2[1]), int(date2[0]))
-
-delta = date1 - date2
-
-if sys.argv[1] != "today" and sys.argv[2] != "today":
-	if int(delta.days) < 0:
-		print(sys.argv[1], "is", int(delta.days) * -1, "days before", sys.argv[2])
-	elif int(delta.days) > 0:
-		print(sys.argv[1], "is", int(delta.days) * -1, "days after", sys.argv[2])
+	if sys.argv[1] == "today":
+		date1 = str(datetime.date.today()).split('-')
+		date1.reverse()
 	else:
-		print("Those are the same dates!")
-else:
-	if int(delta.days) < 0:
-		print("That date will be in", int(delta.days) * -1 , "days")
-	elif int(delta.days) > 0:
-		print("That date was", int(delta.days) * -1 , "days in the past")
+		date1 = sys.argv[1].split('/')
+
+	if sys.argv[2] == "today":
+		date2 = str(datetime.date.today()).split('-')
+		date2.reverse()
 	else:
-		print("Today is indeed today")
+		date2 = sys.argv[2].split('/')
+
+	if len(date1) > 3 or len(date2) > 3:
+		print("Argument not a date"), exit (-1)
+
+	for i in range(0, len(date1)):
+		if not date1[i].isdigit():
+			print("Bad argument format")
+			exit (0)
+
+	for i in range(0, len(date2)):
+		if not date2[i].isdigit():
+			print("Bad argument format")
+			exit (0)
+
+	date1 = datetime.date(int(date1[2]), int(date1[1]), int(date1[0]))
+	date2 = datetime.date(int(date2[2]), int(date2[1]), int(date2[0]))
+
+	delta = date1 - date2
+
+	if sys.argv[1] != "today" and sys.argv[2] != "today":
+		if int(delta.days) < 0:
+			print(sys.argv[1], "is", int(delta.days) * -1, "days before", sys.argv[2])
+		elif int(delta.days) > 0:
+			print(sys.argv[1], "is", int(delta.days) * -1, "days after", sys.argv[2])
+		else:
+			print("Those are the same dates!")
+	else:
+		if int(delta.days) < 0:
+			print("That date will be in", int(delta.days) * -1 , "days")
+		elif int(delta.days) > 0:
+			print("That date was", int(delta.days) * -1 , "days in the past")
+		else:
+			print("Today is indeed today")
+
+if __name__ == '__main__':
+	main()
